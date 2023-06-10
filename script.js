4f117c866f6cc62af88eb7e0706154a390937fb8
DIPTENDU MANDAL <118509862+diptendu13@users.noreply.github.com>
2023-06-10 12:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+	for (bookIndex in library){
+		if (library[bookIndex].readingStatus === true){
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
